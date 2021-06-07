c1b60c66c3267b094f5254d7deb26fd264a06626
joaobenthin <joaopaulobenthin@hotmail.com>
2021-06-07 19:32:38 -0300

    Utilizando Ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon,
 } from './styles';
 
 export function Dashboard() {
@@ -26,6 +27,8 @@ export function Dashboard() {
               <UserName>João</UserName>
             </User>
           </UserInfo>
+          
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
